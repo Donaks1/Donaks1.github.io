e09b6bf1125b6bca0cfa343f9262871c5e009202
donaks <donaks@tdfygu.com>
2024-12-14 10:28:55 +0800

    tweaked java for menu dropdown

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ document.addEventListener('click', function(event) {
     const isClickInside = dropdown.contains(event.target);
 
     if (!isClickInside) {
-        dropdownContent.style.display = 'none'; // Hide the dropdown
+        dropdownContent.style.display = 'none'; 
     }
 });
 
-// Toggle dropdown display when clicking the button
+
 document.querySelector('.dropdown-btn').addEventListener('click', function(event) {
     const dropdownContent = document.querySelector('.dropdown-content');
     dropdownContent.style.display = 
         dropdownContent.style.display === 'flex' ? 'none' : 'flex';
 
-    event.stopPropagation(); // Prevent event from propagating to document listener
+    event.stopPropagation(); 
 });
